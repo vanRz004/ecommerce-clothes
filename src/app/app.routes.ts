5608557539abbd3b6943c1339413e66b2f0bee63
vanrz004 <anvramirez004@gmail.com>
2024-10-10 10:56:43 -0500

    pipes, routing and url params fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
-import { AboutComponent } from './domains/info/pages/about/about.component';
 
 export const routes: Routes = [
     {
